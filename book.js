41d93172ebbb04954273b166d00833373575eb7f
Jared Mezquita <mezquitaj@gmail.com>
2021-08-18 15:08:32 -0400

    Converted to class obect

--- book.js
+++ book.js
@@ -2,12 +2,15 @@
 let myLibrary = []; //Initialize library, where books are to be stored
 
 //The book constructor
-function Book(title, author, pages, read) {
-    this.title = title,
-    this.author = author,
-    this.pages = pages,
-    this.read = read
-    this.info = function() {
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title,
+        this.author = author,
+        this.pages = pages,
+        this.read = read
+    }
+
+    info() {
         return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`
     }
 }
